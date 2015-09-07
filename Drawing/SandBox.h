2b404a1468f2bbe74eddd737555580332c76d326
Pavlo Dumyak <pavlodumyak@macintosh-2.training.local>
2015-09-07 12:56:10 +0300

    first

---
+++ Drawing/SandBox.h
@@ -0,0 +1,21 @@
+//
+//  SandBox.h
+//  Drawing
+//
+//  Created by Pavlo Dumyak on 9/7/15.
+//  Copyright (c) 2015 Pavlo Dumyak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SandBox : UIView{
+    
+    enum{
+        Triangle,
+        Rectangle,
+        Circle,
+        Romb,
+    }figure;
+}
+
+@end
---
+++ Drawing/SandBox.m
@@ -0,0 +1,27 @@
+//
+//  SandBox.m
+//  Drawing
+//
+//  Created by Pavlo Dumyak on 9/7/15.
+//  Copyright (c) 2015 Pavlo Dumyak. All rights reserved.
+//
+
+#import "SandBox.h"
+
+@implementation SandBox
+
+
+
+
+
+
+
+- (void)drawRect:(CGRect)rect {
+  
+    
+    
+    
+}
+
+
+@end
